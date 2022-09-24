853a12ce162a12a66e3a28b3918b0c56309ecd93
Ramie Mosely <ramiemosely5@gmail.com>
2022-09-23 21:17:41 -0400

    added Game

---
+++ __settings__.js
@@ -0,0 +1,23 @@
+ASSET_PREFIX = "";
+SCRIPT_PREFIX = "";
+SCENE_PATH = "1541504.json";
+CONTEXT_OPTIONS = {
+    'antialias': true,
+    'alpha': true,
+    'preserveDrawingBuffer': false,
+    'preferWebGl2': true,
+    'powerPreference': "default"
+};
+SCRIPTS = [ 103276729, 103276737, 103276739, 103276753, 103276756 ];
+CONFIG_FILENAME = "config.json";
+INPUT_SETTINGS = {
+    useKeyboard: true,
+    useMouse: true,
+    useGamepads: true,
+    useTouch: true
+};
+pc.script.legacy = false;
+PRELOAD_MODULES = [
+    {'moduleName' : 'BASIS', 'glueUrl' : 'files/assets/103276726/1/basis.wasm.js', 'wasmUrl' : 'files/assets/103276727/1/basis.wasm.wasm', 'fallbackUrl' : 'files/assets/103276725/1/basis.js', 'preload' : false},
+    {'moduleName' : 'Ammo', 'glueUrl' : 'files/assets/103276722/1/ammo.wasm.js', 'wasmUrl' : 'files/assets/103276723/1/ammo.wasm.wasm', 'fallbackUrl' : 'files/assets/103276721/1/ammo.js', 'preload' : true},
+];
